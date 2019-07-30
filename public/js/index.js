68bbea1ac9cbd34b220e6ca8f87bec952f689252
RoLoDeXx <dexxrolo@gmail.com>
2019-07-30 22:10:26 +0530

    async_await added

--- public/js/index.js
+++ public/js/index.js
@@ -2,28 +2,32 @@ let container = document.getElementById("container");
 document.addEventListener("submit", e => {
   e.preventDefault();
 
-  let cards = document.getElementsByClassName("card");
-
-  let imgs = document.getElementsByClassName("card-img-top");
-  let titles = document.getElementsByClassName("card-title");
-  let descriptions = document.getElementsByClassName("card-text");
-  let linkToArticles = document.getElementsByClassName("btn");
+  let cards = document.getElementsByClassName("card"),
+    imgs = document.getElementsByClassName("card-img-top"),
+    titles = document.getElementsByClassName("card-title"),
+    descriptions = document.getElementsByClassName("card-text"),
+    linkToArticles = document.getElementsByClassName("btn");
 
   for (let i = 0; i < 20; i++) {
     imgs[i].setAttribute("src", "./imgs/loading.png");
   }
 
   phrase = document.querySelector("input").value;
-  fetch("/news?q=" + phrase).then(response => {
-    response.json().then(data => {
-      for (let i = 0; i < 20; i++) {
-        cards[i].style.display = "block";
-        imgs[i].setAttribute("src", data[i].urlToImage);
-        titles[i].textContent = data[i].title;
-        descriptions[i].textContent = data[i].description;
-        linkToArticles[i].setAttribute("href", data[i].url);
-        linkToArticles[i].textContent = "Show more";
-      }
-    });
+
+  fetchNews(phrase).then(data => {
+    for (let i = 0; i < 20; i++) {
+      cards[i].style.display = "block";
+      imgs[i].setAttribute("src", data[i].urlToImage);
+      titles[i].textContent = data[i].title;
+      descriptions[i].textContent = data[i].description;
+      linkToArticles[i].setAttribute("href", data[i].url);
+      linkToArticles[i].textContent = "Show more";
+    }
   });
 });
+
+async function fetchNews(phrase) {
+  let response = await fetch("/news?q=" + phrase);
+  let data = response.json();
+  return data;
+}
