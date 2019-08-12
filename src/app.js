fcb28a65923f17be75e54ffcdd9705cd01519fc6
RoLoDeXx <dexxrolo@gmail.com>
2019-08-12 19:56:13 +0530

    refactoring w hbs complete

--- src/app.js
+++ src/app.js
@@ -13,19 +13,17 @@ hbs.registerPartials(path.join(__dirname, "../templates/partials"));
 app.set("views", path.join(__dirname, "../templates/views"));
 app.use(express.static(path.join(__dirname, "../public")));
 
-app.get("", (req, res) => {
-  res.render("index");
-});
-
 app.get("/about", (req, res) => {
   res.render("about");
 });
 
-app.get("/news", (req, res) => {
-  if (!req.query.q) return res.send("add a phrase");
+app.get("/", (req, res) => {
+  // if (!req.query.q) return res.send("add a phrase");
   NewsArticles(req.query.q, (error, response) => {
     if (error) return console.log("error in news");
-    res.send(response);
+    res.render("index", {
+      news: response
+    });
   });
 });
 
